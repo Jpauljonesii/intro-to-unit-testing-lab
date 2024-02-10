00979857d0f200eaa6332e94745f2e3edeb15d8e
John P. Jones II <jpauljonesii@gmail.com>
2024-02-10 11:04:51 -0600

    made updates

--- utility.test.js
+++ utility.test.js
@@ -25,3 +25,25 @@ describe("isPalindrome", () => {
     expect(isPalindrome("A man, a plan, a canal, Panama")).toBe(true);
   });
 });
+
+
+describe("fibonacci", () => {
+  test("When a zero is entered it should return a zero", () => {
+    
+    expect(fibonacci(number)).toBe(false);
+  });
+
+  /*test("should return false for a non-fibonacci number", () => {
+    expect(fibonacci("hello")).toBe(false);
+  });
+
+  test("should be case insensitive", () => {
+    expect(fibonacci("RaceCar")).toBe(true);
+  });
+
+  test("should ignore spaces, punctuation, and special characters", () => {
+    expect(fibonacci("A man, a plan, a canal, Panama")).toBe(true);
+  });
+});
+*/
+
